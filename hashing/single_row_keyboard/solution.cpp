4c523d633aa1406307bfc89bd3dd60390e4177b6
TravHaran <ratnahat@mcmaster.ca>
2024-07-14 21:52:40 -0400

    completed lc 1165

--- hashing/single_row_keyboard/solution.cpp
+++ hashing/single_row_keyboard/solution.cpp
@@ -1,9 +1,20 @@
 #include <string>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     int calculateTime(string keyboard, string word) {
-        
+        unordered_map<char, int> idx_map;
+        for(int i = 0; i < keyboard.size(); i++){
+            idx_map[keyboard[i]] = i;
+        }
+        int time(0);
+        int prev_idx = 0;
+        for(auto& el: word){
+            time += abs(prev_idx - idx_map[el]);
+            prev_idx = idx_map[el];
+        }
+        return time;
     }
 };
